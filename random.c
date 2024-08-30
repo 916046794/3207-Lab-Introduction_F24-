c56044f6398c95516c572bbdfea4e527ed138b53
916046794 <tuo74327@temple.edu>
2024-08-29 20:35:32 -0400

    extra variable 4 readability

--- random.c
+++ random.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 
 char randchar(){
+	int asciiIndex = 0;
 	int asciiCode = 0;
 	char returnChar;
 
 	srand(time(NULL));
 	//seeding it so that it's truly random
-	asciiCode = rand() % ((25+1-0) + 0);
+	asciiIndex = rand() % ((25+1-0) + 0);
 	//getting a random number between 0-25 that I can use for ASCII code uppercase letters
-	returnChar = asciiCode+65;
+
+	asciiCode = asciiIndex+65;
+	returnChar = asciiCode;
 
 	return returnChar;
 }
