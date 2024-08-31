ad3e27c415287d167c88c436cd3f9c0192177e31
916046794 <tuo74327@temple.edu>
2024-08-31 15:49:47 -0400

    updated code has libraries

--- random.c
+++ random.c
@@ -1,18 +1,16 @@
-#include <time.h>
-#include <stdlib.h>
-
 char randchar(){
 	int asciiIndex = 0;
 	int asciiCode = 0;
 	char returnChar;
 
-	srand(time(NULL));
-	//seeding it so that it's truly random
 	asciiIndex = rand() % ((25+1-0) + 0);
 	//getting a random number between 0-25 that I can use for ASCII code uppercase letters
 
 	asciiCode = asciiIndex+65;
 	returnChar = asciiCode;
+	//adding said index to the base ASCII for A of 65, then typecasting
+	
+	//printf("%c", returnChar);
 
 	return returnChar;
 }
